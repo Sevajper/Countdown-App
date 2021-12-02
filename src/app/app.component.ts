f77a12f10cd1118b8fa9878191bb90d51f2327c8
Sevajper <stefanos.bampovits@gmail.com>
2021-12-02 14:00:35 +0100

    Add comments, fix performance, limit input fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   countdown = ''
 
   ngOnInit() {
-    /* If local storage exists fetch data from local storage
+    /* If local storage exists get data from local storage
       otherwise set default data */
     if (localStorage.getItem('title') || localStorage.getItem('date')) {
       this.title = localStorage.getItem('title') as string
@@ -22,50 +22,49 @@ export class AppComponent implements OnInit {
       localStorage.setItem('date', '2021-12-31')
     }
 
-    /* Called once before interval to not wait for 1 second before showing countdown */
+    // Called once before interval to not wait for 1 second before showing countdown and calculating font size
     this.getCountdownFromDate()
+    this.calculateFontSize()
 
     setInterval(() => {
       this.getCountdownFromDate()
+      // Calculating font size every second because countdown can go from 2 digits to 1 and vice verca
+      this.calculateFontSize()
     }, 1000)
   }
 
-  ngAfterContentInit() {
-    this.calculateTitleFontSize()
-  }
-
   setTitle(inputTitle: string) {
-    this.title = inputTitle
-    localStorage.setItem('title', this.title)
-
-    this.calculateTitleFontSize()
+    // Limit input to 100 chars
+    if (inputTitle.length <= 100) {
+      this.title = inputTitle
+      localStorage.setItem('title', this.title)
+      this.calculateFontSize()
+    }
   }
 
   setDate(inputDate: string) {
-    this.eventDate = inputDate
-    localStorage.setItem('date', this.eventDate)
+    // Limit input to date format length (YYYY-MM-DD)
+    if (inputDate.length <= 10) {
+      this.eventDate = inputDate
+      localStorage.setItem('date', this.eventDate)
+      this.calculateFontSize()
+    }
   }
 
   /**
-   * Calculates time between user input date with time set to midnight 00:00
-   * and the date and time right now. Then displays countdown
+   * Calculates time between user input date and time set to midnight 00:00 (default)
+   * and the date and time right now. Then displays sets countdown value
    */
   getCountdownFromDate() {
+    // Special case for iOS since it seems to change month-day order in the string
     const dateRegexValidationIOS =
       /(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|[1][0-2])\/[0-9]+,\s[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?/i
     const defaultDateRegexValidation =
       /(0?[1-9]|[1][0-2])\/(0?[1-9]|[12][0-9]|3[01])\/[0-9]+,\s[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?/i
 
-    const year = parseInt(this.eventDate.substring(0, 4))
-
-    const month = parseInt(this.eventDate.substring(5, 7))
-    const day = parseInt(this.eventDate.substring(8, 10))
-    console.log(year, month, day)
-
     const format = 'short'
     const myDate = new Date(`${this.eventDate}T00:00:00`)
     const locale = 'en-US'
-    console.log(myDate.toLocaleString())
 
     if (
       dateRegexValidationIOS.test(myDate.toLocaleString()) ||
@@ -98,18 +97,34 @@ export class AppComponent implements OnInit {
   }
 
   /**
-   * Expands child width to parent width by increasing font size of childs text
-   * by 0.5px until the width is the same
+   * Expands child span width to parent div width by increasing
+   * font size of childs text by 2px and after by 0.01px until the
+   * width is the same
    */
-  calculateTitleFontSize() {
+  calculateFontSize() {
     setTimeout(() => {
       const parent =
         document.querySelectorAll<HTMLElement>('.titleContainer')[0]
       const children = parent.querySelectorAll<HTMLElement>('span')
-      children[0].style.fontSize = '1px'
-      children[1].style.fontSize = '1px'
+
+      let fontSizePx = 0
+
       children.forEach((child) => {
+        // If initial value does not exist (first render) set inital value to 300px
+        // otherwise transform the font vw to px for further calculations
+        if (!child.style.fontSize) {
+          child.style.fontSize = '300px'
+        } else {
+          fontSizePx =
+            (document.documentElement.clientWidth *
+              parseFloat(child.style.fontSize)) /
+            100
+
+          child.style.fontSize = `${fontSizePx}px`
+        }
+
         if (parent.offsetWidth && child.offsetWidth) {
+          // First two while checks get the fontSize close to how big it needs to be without caring too much for accuracy
           while (parent.offsetWidth > child.offsetWidth) {
             child.style.fontSize = `${parseFloat(child.style.fontSize) + 2}px`
           }
@@ -118,17 +133,28 @@ export class AppComponent implements OnInit {
             child.style.fontSize = `${parseFloat(child.style.fontSize) - 2}px`
           }
 
+          // Second two while checks get the fontSize to max width of container with really good accuracy
           while (parent.offsetWidth > child.offsetWidth) {
-            child.style.fontSize = `${parseFloat(child.style.fontSize) + 0.1}px`
+            child.style.fontSize = `${
+              parseFloat(child.style.fontSize) + 0.01
+            }px`
           }
 
-          const fontSize =
+          while (parent.offsetWidth < child.offsetWidth) {
+            child.style.fontSize = `${
+              parseFloat(child.style.fontSize) - 0.01
+            }px`
+          }
+
+          // Transform font size from px to vw
+          const fontSizeVw =
             (100 * parseFloat(child.style.fontSize)) /
             document.documentElement.clientWidth
 
-          child.style.fontSize = `${fontSize}vw`
+          child.style.fontSize = `${fontSizeVw}vw`
         }
       })
     }, 10)
+    clearTimeout()
   }
 }
